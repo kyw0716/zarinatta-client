64634e5e17cb3225ba817a1e7ebb212308c29e62
kyw0716 <kyw0716@naver.com>
2025-02-09 03:59:03 +0000

    test: 로그인 쿼리 데이터 어떻게 뜨는지 확인용 배포

--- src/components/header/LoginButton.tsx
+++ src/components/header/LoginButton.tsx
@@ -1,21 +1,31 @@
-'use client';
+"use client";
 
-import { useLoginRedirectCodeQuery, useLogoutMutation } from '@/hooks/query/use-login';
-import { userStore } from '@/store/userStore';
-import { useRouter } from 'next/navigation';
+import {
+  useLoginRedirectCodeQuery,
+  useLogoutMutation,
+} from "@/hooks/query/use-login";
+import { userStore } from "@/store/userStore";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
 export default function LoginButton() {
   const router = useRouter();
+  const queryClient = useQueryClient();
   const { data: redirectUri } = useLoginRedirectCodeQuery();
   const { mutate: logoutMutation } = useLogoutMutation();
   const userInformation = userStore(({ userInfo }) => userInfo);
-  const setUserInfo = userStore((state) => state.setUserInfo);
   const resetUserInfo = userStore((state) => state.resetUserInfo);
+  const loginQueryData = queryClient.getQueryData(["loginQuery"]);
+
+  console.log(loginQueryData);
 
   const redirectToLoginPage = () => {
     if (redirectUri === undefined) return;
 
-    sessionStorage.setItem('pathNameBeforeClickLoginButton', window.location.pathname);
+    sessionStorage.setItem(
+      "pathNameBeforeClickLoginButton",
+      window.location.pathname
+    );
     router.push(redirectUri);
   };
 
@@ -24,7 +34,10 @@ export default function LoginButton() {
     resetUserInfo();
   };
 
-  if (userInformation.userEmail !== undefined && userInformation.userNick !== undefined)
+  if (
+    userInformation.userEmail !== undefined &&
+    userInformation.userNick !== undefined
+  )
     return <span onClick={logout}>로그아웃</span>;
 
   return <span onClick={redirectToLoginPage}>로그인</span>;
